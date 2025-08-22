e4785eb28bdd41e31ea94c79c4b969bb17bf780a
novjex <barivishit28@gmail.com>
2025-08-22 09:52:30 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
     <section className="relative bg-transparent font-sans flex flex-col justify-start h-screen">
 
       {/* Content Section */}
-      <div className="relative text-center max-w-3xl px-4 sm:px-6 pt-10 md:pt-24 lg:pt-32 pb-10 z-10 mx-auto">
+      <div className="relative text-center max-w-3xl px-4 sm:px-6 pt-10 md:pt-24 lg:pt-32 pb-40 z-10 mx-auto">
         <h1 className="text-3xl sm:text-5xl md:text-6xl font-extrabold text-white leading-tight tracking-tighter mb-6 shadow-lg">
           Glow with Focus, Grow with Knowledge
         </h1>
