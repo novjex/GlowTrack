7f0ef8b1624edf825a1a80189b0621f4b905bf13
novjex <barivishit28@gmail.com>
2025-08-22 09:55:29 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,12 +6,12 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <section className="relative bg-transparent font-sans flex flex-col justify-start h-screen">
+    <section className="relative bg-transparent font-sans flex flex-col justify-start min-h-screen">
 
       {/* Content Section */}
       <div className="relative text-center max-w-3xl px-4 sm:px-6 pt-10 md:pt-24 lg:pt-32 pb-40 z-10 mx-auto">
         <h1 className="text-3xl sm:text-5xl md:text-6xl font-extrabold text-white leading-tight tracking-tighter mb-6 shadow-lg">
-          Glow with Focus, Grow with Knowledge!
+          Glow with Focus, Grow with Knowledge
         </h1>
 
         <p className="text-base sm:text-lg md:text-xl text-gray-200 max-w-2xl mx-auto mb-8 font-light opacity-90">
