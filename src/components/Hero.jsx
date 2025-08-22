c5a3c226720a230364f02e97e408a890f433a56f
novjex <barivishit28@gmail.com>
2025-08-22 09:53:09 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
       {/* Content Section */}
       <div className="relative text-center max-w-3xl px-4 sm:px-6 pt-10 md:pt-24 lg:pt-32 pb-40 z-10 mx-auto">
         <h1 className="text-3xl sm:text-5xl md:text-6xl font-extrabold text-white leading-tight tracking-tighter mb-6 shadow-lg">
-          Glow with Focus, Grow with Knowledge
+          Glow with Focus, Grow with Knowledge!
         </h1>
 
         <p className="text-base sm:text-lg md:text-xl text-gray-200 max-w-2xl mx-auto mb-8 font-light opacity-90">
